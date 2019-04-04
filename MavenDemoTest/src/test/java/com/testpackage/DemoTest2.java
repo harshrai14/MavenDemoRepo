f5c33f6c283f7634eade81ecfec9d739c57e5eb6
harshrai14 <harshrai12345@gmail.com>
2019-04-05 00:53:09 +0530

    Demo commit

---
+++ MavenDemoTest/src/test/java/com/testpackage/DemoTest2.java
@@ -0,0 +1,35 @@
+package com.testpackage;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+
+	@Test
+	public void sum() {
+		System.out.println("SUM");
+		int a=10,b=20;
+		Assert.assertEquals(a+b, 30);	
+	}
+	
+	@Test
+	public void div() {
+		System.out.println("DIVIDE");
+		int a=10,b=20;
+		Assert.assertEquals(b/a,2);	
+	}
+	
+	@Test
+	public void mul() {
+		System.out.println("MULTIPLY");
+		int a=10,b=20;
+		Assert.assertEquals(a*b, 200);	
+	}
+	
+	@Test
+	public void sub() {
+		System.out.println("SUBTRACT");
+		int a=10,b=20;
+		Assert.assertEquals(b-a, 10);	
+	}
+}
